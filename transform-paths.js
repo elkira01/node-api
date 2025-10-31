055a7d9ff2fdafda060574e3bfb68d6e735d0910
elkira01 <elkira01@gmail.com>
2025-10-31 03:01:14 +0100

    try resolve aliases issues

---
+++ transform-paths.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const fs = require('fs')
+const tsconfig = require('./tsconfig.json')
+
+module.exports = function (fileInfo, api) {
+    const j = api.jscodeshift
+    const root = j(fileInfo.source)
+    const filePath = fileInfo.path
+
+    // Get paths from tsconfig
+    const paths = tsconfig.compilerOptions.paths || {}
+
+    root.find(j.ImportDeclaration).forEach((path) => {
+        const importPath = path.node.source.value
+
+        // Check each alias
+        Object.keys(paths).forEach((alias) => {
+            const aliasPattern = alias.replace('/*', '')
+            const targetPattern = paths[alias][0].replace('/*', '')
+
+            if (importPath.startsWith(aliasPattern)) {
+                const relativePath = importPath.replace(
+                    aliasPattern,
+                    targetPattern
+                )
+                const absoluteImport = require('path').resolve(
+                    process.cwd(),
+                    relativePath
+                )
+                const fileDir = require('path').dirname(filePath)
+                let relPath = require('path').relative(fileDir, absoluteImport)
+
+                if (!relPath.startsWith('.')) {
+                    relPath = './' + relPath
+                }
+
+                path.node.source.value = relPath
+            }
+        })
+    })
+
+    return root.toSource()
+}
